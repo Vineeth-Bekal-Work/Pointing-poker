115cd1831bc6cec081b49d2d49c05faee101e6d7
Vineeth-Bekal-Work <vineeth.bekal@impelsys.com>
2021-09-24 10:42:44 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 
 import './App.css';
 import Deck from './cards';
-import Chat from './chatbox';
 
 function App() {
   return (
     <div className="App">
       <Deck/>
-      <Chat/>
     </div>
   );
 }
