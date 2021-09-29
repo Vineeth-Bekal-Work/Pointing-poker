556f417bb5955c378afa58522bd63931ae0ddb41
Vineeth-Bekal-Work <vineeth.bekal@impelsys.com>
2021-09-29 09:38:51 +0530

    Server Changes minor

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ io.on('connect', (socket) => {
         socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.`});
         socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
 
-        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+        //io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
 
         callback();
     });
@@ -54,7 +54,7 @@ io.on('connect', (socket) => {
 
         if(user) {
         io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
-        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+        //io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
         }
     })
 });
